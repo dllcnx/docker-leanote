6234b9dbb1eed51b31ffe0b3a6fc9b4e7fb95ab4
LLCN <jkf19980216@qq.com>
2020-02-10 13:13:30 +0800

    Update get-latest.sh

--- latest/get-latest.sh
+++ latest/get-latest.sh
@@ -1,6 +1,6 @@
 #! bin/sh
 rm -rf leanote app.conf leanote_cache views && mkdir leanote
-git clone https://github.com/leanote/leanote.git leanote_cache
+https://github.com/KeiferJu/leanote.git leanote_cache
 mv ./leanote_cache/conf/app.conf ./ && mv ./leanote_cache/app/views ./ && mv ./leanote_cache/public ./leanote && mv ./leanote_cache/mongodb_backup ./leanote && mkdir ./leanote/files && mkdir ./leanote/public/upload
 rm -rf leanote_cache
 sed -i 's/db.host=127.0.0.1/db.host=mongo/g' ./app.conf
